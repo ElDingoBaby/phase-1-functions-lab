6f32c262d60f5a6ac34d7508d50aed5c88ac9dee
Alexis Sanchez <alexiss574@gmail.com>
2023-01-24 15:38:18 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+const scuberHq = 42
+const blocksDistanceInFeet = 264
+
+
+function distanceFromHqInBlocks(someValue) {
+    let numberOfBlocks = Math.abs(scuberHq - someValue)
+    return numberOfBlocks
+
+}
+
+
+function distanceFromHqInFeet(someValue) {
+    let distanceInFeet = Math.abs(scuberHq - someValue) * blocksDistanceInFeet
+    return distanceInFeet
+
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let totesDistanceInFeet = Math.abs(start - destination) * blocksDistanceInFeet
+    return totesDistanceInFeet
+}
+
+function calculatesFarePrice(start, destination) {
+    let fareCost = Math.abs(start - destination) * blocksDistanceInFeet;
+    if (fareCost < 400) {
+        return fareCost = 0
+    } else if (fareCost < 2000) {
+        fareCost = Math.abs(fareCost - 400) * 0.02
+        return fareCost
+    } else if (fareCost < 2500) {
+        fareCost = Math.abs(fareCost - 400)
+        return fareCost = 25
+    } else if (fareCost > 2500) {
+        fareCost = Math.abs(fareCost - 400)
+        return `cannot travel that far`
+    }
+
+}
